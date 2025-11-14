2e54c2b4e2533559a75e1147e93550e1166a29ae
Jenkins CI <riaz11@hotmail.com>
2025-11-14 02:39:39 -0500

    updates

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,6 +3,8 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!
+
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Missing Supabase environment variables');
