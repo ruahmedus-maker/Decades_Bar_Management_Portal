dfbd84a815e15dee3dcd80e09389d3b579f55c56
Jenkins CI <riaz11@hotmail.com>
2026-04-14 01:55:04 -0400

    feat: enable client-side rendering for the home page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect } from 'react';
 import { AppProvider, useApp } from '@/contexts/AppContext';
 import LoginBarrier from '@/components/LoginBarrier';
